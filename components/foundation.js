b888b7f21402387bc92844f4fffb096c95c4f0ad
lol-russo <jack.lorusso@digivizer.com>
2017-03-21 20:23:36 +1100

    Set up re-usable color and scale values

--- components/foundation.js
+++ components/foundation.js
@@ -1,1 +1,27 @@
-// All reusable values go here in objects eg. colors, modular scale
+const colors = {
+  primaryPurple : '354270',
+}
+
+const scale = {
+  ms_6 : '0.178em',
+  ms_5 : '0.238em',
+  ms_4 : '0.317em',
+  ms_3 : '0.422em',
+  ms_2 : '0.563em',
+  ms_1 : '0.75em',
+  ms0  : '1em',
+  ms1  : '1.333em',
+  ms2  : '1.777em',
+  ms3  : '2.369em',
+  ms4  : '3.157em',
+  ms5  : '4.209em',
+  ms6  : '5.61em',
+  ms7  : '7.478em',
+  ms8  : '9.969em',
+  ms9  : '13.288em',
+  ms10 : '17.713em',
+  ms11 : '23.612em',
+  ms12 : '31.475em'
+};
+
+export { colors, scale }
