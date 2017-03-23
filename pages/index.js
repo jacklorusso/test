8f2e231c7f85da5bdda9e2a01d072ae386c3fd80
lol-russo <jack.lorusso@digivizer.com>
2017-03-23 14:21:24 +1100

    Inject system fonts as global font-family for now

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { colors, scale } from '../components/foundation'
 
 injectGlobal`
 	body {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 		margin: 0;
     padding: ${scale.ms0};
     background: linear-gradient(
@@ -20,7 +21,7 @@ injectGlobal`
 
 const ContentArea = styled.div`
   width: 100%;
-  height: calc(100vh);
+  height: calc(100vh - ${scale.ms0} - ${scale.ms0});
   background: ${colors.primaryPurple};
 `
 export default class Index extends React.Component {
@@ -37,7 +38,6 @@ export default class Index extends React.Component {
           ]}
         />
         <ContentArea>
-
         </ContentArea>
       </div>
     )
